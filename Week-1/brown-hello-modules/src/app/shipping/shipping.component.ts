349f968d95bc21269f142c2dbafaab7d98e5ede9
Jared Brown <brownjt01@sunyjcc.edu>
2018-07-15 16:34:38 -0400

    Added Exercise 1.5

---
+++ Week-1/brown-hello-modules/src/app/shipping/shipping.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  template: `
+    <p>
+      This is the Shipping Template in shipping.components.ts
+    </p>
+  `,
+  styles: []
+})
+export class ShippingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
