bdacfc0a6ddef5cb5418d20422dce77a57c278a8
Jared Brown <brownjt01@sunyjcc.edu>
2018-07-22 13:58:03 -0400

    Added Exercise 2.2

---
+++ Week-2/one-way-binding/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '\
+  <h1>{{name}}</h1>\
+  <button (click)="changeName()">Change Name</button>\
+  ',
+  styles: [
+    '\
+      h1{\
+        color: red;\
+      }\
+    '  
+  ]
+
+})
+export class AppComponent {
+  title = 'app';
+
+  name:string = "Jared Brown";
+
+  changeName(){
+    this.name = "Mr. Brown";
+  }
+}
