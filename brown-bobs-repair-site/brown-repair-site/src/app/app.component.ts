7e0771fcc3afd5d6ccfff9a39c78e7d472d6d785
Jared Brown <brownjt01@sunyjcc.edu>
2018-09-09 16:22:37 -0400

    Added Bobs Repair Shop Project

---
+++ brown-bobs-repair-site/brown-repair-site/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { SwitcherService } from './switcher.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [SwitcherService]
+})
+export class AppComponent {
+  title = 'app'; 
+  constructor(public switchService: SwitcherService){
+  }
+
+   
+}
